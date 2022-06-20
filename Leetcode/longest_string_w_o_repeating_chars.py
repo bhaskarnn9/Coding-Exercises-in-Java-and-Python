507397e46dc98f255b12d349742fc3154ee8f4b6
bhaskarnn9 <bneella@outlook.com>
2022-06-20 15:46:08 -0500

    longest string without repeating characters

--- Leetcode/longest_string_w_o_repeating_chars.py
+++ Leetcode/longest_string_w_o_repeating_chars.py
@@ -1,37 +1,31 @@
 from collections import deque
-from operator import le
 
 
-nums = [205, 321, 190, 180, 48, 110, 148, 83, 469, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ,36, 193, 115, 209, 38, 24, 113, 424, 230, 463, 271, 398, 434, 19, 471, 
-489, 449, 368, 212, 307, 76, 467, 34, 333, 412, 459, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 234, 286, 22, 7, 146, 141, 192, 323, 367, 81, 414, 443, 68, 392, 370, 
-309, 152, 133, 413, 89, 118, 332, 365, 92, 482, 127, 11, 52, 411, 119, 2, 473, 138, 441, 174, 64, 74, 401, 35, 95, 18, 227, 
-54, 289, 317, 396, 20, 322, 372, 360, 130, 70, 447, 451, 163, 394, 295, 223, 458, 287, 116, 366, 123, 348, 140, 1 ,8 ,7 ,6 ,5 ,4 ,3 ,2 ,1]
-
-def longest_sequence(nums):
-
-    longest_sequence = []
-
-    for i in range(1, len(nums)):
-        seq = deque()
-        if nums[i] == nums[i-1] + 1:
-            seq.appendleft(nums[i-1])
-        else:
+s = 'ckilbkd'
+def longest(s):
+    longest = []
+    temp = None
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
             continue
-
-        while nums[i] == nums[i-1] + 1:
-            if i == len(nums):
-                break
-            seq.append(nums[i])
-            i += 1
+        else:
+            temp = str(s[i-1])
         
-        if len(longest_sequence) < len(seq):
-            longest_sequence = seq
-
+        while s[i] != s[i-1]:
+            if s[i] not in temp:
+                temp = temp + s[i]
+            else:
+                break
+            i += 1
+            if i == len(s):
+                break
+        
+        longest.append(temp)
     
-    print(longest_sequence)
-    return longest_sequence
+    longest = sorted(longest, key=len, reverse=True)[:1]
+    if not longest:
+        return len(s[0])
+    print(longest[0])
+    return len(longest[0])
 
-longest_sequence(nums)
-
-
-
+print(longest(s))
